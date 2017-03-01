281117e4e1ffe1b3e1209b5d2c24b2d0e450d7a0
Asma Chaima <achaima@spartaglobal.co>
2017-03-01 15:05:26 +0000

    Looking at DOM

---
+++ week03-day03-dom-events-jquery/main.js
@@ -0,0 +1,40 @@
+console.log('in main.js');
+
+console.log(document); //This puts the whole HTML structure in the inspect console
+console.log(document.body);
+
+console.log(('---using querySelectorAll'));
+
+var selectedListItems = document.querySelectorAll('.selected');
+console.log('selectedListItems:', selectedListItems);
+
+for ( var i = 0 ; i < selectedListItems.length; i++) {
+  console.log('selected list item style:', selectedListItems[i].style);
+  selectedListItems[i].style.color = 'red';
+}
+
+console.log('Create and append a new element');
+
+var newListItem = document.createElement('li'); //this only exists in Javascript land and so nothing appears
+//1.So here we created a list item
+
+newListItem.innerHTML = 'in New York';
+//2. Now you've created content for this element but haven't actually connected either'
+
+//Still at this point haven't done anything to put it in the DOM. We have created and modified it.
+//Theres two stages:
+//create how you want it
+//then insert it
+
+// We are going to call .appendChild() on the parent <ul> element.
+//That <ul> element has an ID attribute
+var listContainer = document.getElementById('item-list');
+//3.Now we know the Id and where to find it and command the computer to get it
+
+listContainer.appendChild(newListItem);
+//4. Now we have the list item we add the command to append child and put in the content we created earlier in newListItem
+//This goes and appends the variable you created early on to the ID
+
+//My Attempt adding a new list item using a class name
+
+newListItem.setAttribute('class','selected');
