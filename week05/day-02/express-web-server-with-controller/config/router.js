408430d79bc0790bd46d4c4408963c7d8cd31edf
Asma Chaima <achaima@spartaglobal.co>
2017-03-14 12:03:24 +0000

    added controller and router

---
+++ week05/day-02/express-web-server-with-controller/config/router.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+var userController =require('../controllers/users-controller');
+
+
+router.route('/users')
+ .get(userController.index)
+ .post(userController.create);
+
+router.get('/users/new', userController.new);
+router.get('/users/:id/edit', userController.edit);
+
+
+router.route('/users/:id')
+.put(userController.update)
+.get(userController.show)
+.delete(userController.destroy);
+
+module.exports = router;
