21f76b5f19206ae236166e63d09788afcb5124a2
Asma Chaima <achaima@spartaglobal.co>
2017-03-15 17:38:21 +0000

    added mongoDB and updated accordingly

--- week05-node/day03/express-web-server-mvc/index.js
+++ week05-node/day03/express-web-server-mvc/index.js
@@ -2,10 +2,15 @@ var express = require('express');
 var router = require('./config/router');
 var bodyParser = require('body-parser');
 var layouts = require('express-ejs-layouts');
+var methodOverride = require('method-override');
+var mongoose = require('mongoose');
 var app = express();
 var port = 3000;
 
+mongoose.connect('mongodb://localhost/sg-mv');
+
 app.set('view engine', 'ejs');
+
 app.use(function (req, res, next) {
   // simple middleware logging
   console.log(req.method, req.path);
@@ -14,6 +19,17 @@ app.use(function (req, res, next) {
 app.use(layouts);
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+app.use(methodOverride(function(req){
+  console.log('methodOverride check: re.body: ', req.body);
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+    //look in urlencoded POST bodies and delete it
+    const method = req.body._method;
+    delete req.body_method;
+    return method;
+  }
+}));
+
 
 app.use(router);
 
