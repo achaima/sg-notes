6d60749fb57d7a763f2d260f652988961ccde70f
Asma Chaima <achaima@spartaglobal.co>
2017-03-30 14:55:49 +0100

    add home, about page, route and controller

---
+++ week07-day04-angular-routing/angular-states/angular-states-starter-code/js/README.md
@@ -0,0 +1,5 @@
+Route: #/users - The URL bit
+State: 'show-users'
+
+We are going to be specifying and mapping a route in the URL and an actual state.
+For success we would say give it bla bla bla state e.g. 'user-create-new'.
--- week07-day04-angular-routing/angular-states/angular-states-starter-code/js/app.js
+++ week07-day04-angular-routing/angular-states/angular-states-starter-code/js/app.js
@@ -1,2 +1,17 @@
+function mainRouter($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state('home', {
+      url: '/',
+      templateUrl: '../states/home.html'
+    })
+    .state('about', {
+      url: '/about',
+      templateUrl: '../states/about.html'
+    });
+
+  $urlRouterProvider.otherwise('/');
+}
+
 angular
-  .module('angularstates', []);
+  .module('angularstates', ['ui.router'])
+  .config(mainRouter);
