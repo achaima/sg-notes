f060e05022c0b5abc0e5f0e42c221b1d8061cde4
Asma Chaima <achaima@spartaglobal.co>
2017-04-03 12:33:26 +0100

    add use of link function in directives(toggle)

--- week08-day01-angular-directives/angular-directives/directives/profile/profile.directive.js
+++ week08-day01-angular-directives/angular-directives/directives/profile/profile.directive.js
@@ -7,8 +7,17 @@ angular
       templateUrl: 'directives/profile/profile.directive.html',
       scope: {
         person: '='
+      },
+      link: function (scope/*, element, attrs*/) {
+        scope.showSkills = false;
+
+        scope.toggleSkills = function() {
+          scope.showSkills = !scope.showSkills;
+        };
       }
     };
   });
 
   //'E' is shorthand for element - it means this directory has to be implements by an element. Other directives include attributes.
+//attrs short for attributes
+//link allows us to view ..... for the method
