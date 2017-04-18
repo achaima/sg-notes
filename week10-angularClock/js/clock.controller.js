767c745d0a6a7a88a46057881015e5d97cec3fb4
Asma Chaima <achaima@spartaglobal.co>
2017-04-18 09:39:45 +0100

    made clock function got rid off css created clock

--- week10-angularClock/js/clock.controller.js
+++ week10-angularClock/js/clock.controller.js
@@ -1,14 +1,38 @@
-function clockController($timeout) {
+function clockController($interval){
   var controller = this;
 
   controller.tick = function() {
     controller.date = new Date();
-    $timeout(controller.tick, 1000);
+    controller.seconds = controller.date.getUTCSeconds();
+    controller.minutes = controller.date.getUTCMinutes();
+    controller.hours = controller.date.getUTCHours() + '1';
+    //Set the time and add an hour as GMT is an hour behind
+    controller.secondsHandAngle = controller.seconds * 6;
+    controller.minutesHandAngle = controller.minutes * 6;
+    //move 6 degrees every minute or second 360/60 = 6
+    controller.hoursHandAngle = controller.hours * 30 + (controller.minutes * 0.5);
+    //360deg divided by 12 = 30deg in 1 hour
+    //0.5deg every min 30deg/60min = 0.5deg per minute
+
+
+//css animation applying to the hands with ng-style in the HTML page
+    controller.secondsHandStyle = {
+      'transform': `rotate(${controller.secondsHandAngle}deg)`
+    };
+    controller.minutesHandStyle = {
+      'transform': `rotate(${controller.minutesHandAngle}deg)`
+    };
+    controller.hoursHandStyle = {
+      'transform': `rotate(${controller.hoursHandAngle}deg)`
+    };
   };
-  controller.tick();
+
+  function init() {
+    $interval(controller.tick, 1000);
+  }
+
+  init();
 }
-
-
 
 
 angular
